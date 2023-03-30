b4f87381ae302ef791eb8ff125dbbc4bcf1b6f60
Maxime Maillot <maillot.maxime62@gmail.com>
2023-03-30 12:00:07 +0200

    finish pendu

--- CsharpAvance/Pendu/Program.cs
+++ CsharpAvance/Pendu/Program.cs
@@ -1,30 +1,39 @@
 ﻿using Pendus.Classes;
 
+void WriteInColor(string msg, ConsoleColor color)
+{
+    Console.ForegroundColor = color;
+    Console.WriteLine(msg);
+    Console.ResetColor();
+};
+
+
 Console.WriteLine("=== Paramètre de partie ===");
-Console.Write("Voulez-vous un nombre d'essais spécifique (10 par défaut) ? ");
+Console.Write("Voulez-vous un nombre de chances spécifique (10 par défaut) ? y/n ");
 string choice = Console.ReadLine().Trim().ToLower();
 
+int nbEssais = 10;
 if (choice == "y")
 {
     bool isCorrect;
-    int nbEssais;
     do
     {
-        Console.Write("Combien voulez-vous d'essais ? ");
+        Console.Write("Combien voulez-vous de chances ? ");
         isCorrect = int.TryParse(Console.ReadLine(), out nbEssais);
         if (nbEssais < 1)
         {
-            Console.WriteLine("Entrez un nombre d'essai correct supérieur à 0");
+            Console.WriteLine("Entrez un nombre de chance correct supérieur à 0");
         }
     } while (!isCorrect);
 }
-Pendu pendu = new Pendu();
+Pendu pendu = new Pendu(nbEssais);
 
-bool hasWin = false;
+bool hasWin;
 do
 {
+    //Console.Clear();
     Console.WriteLine($"Le mot à trouver :  {pendu.Masque}");
-    Console.WriteLine($"Il vous reste {pendu.NbEssais}");
+    Console.WriteLine($"Il vous reste {pendu.NbEssais} chances");
     bool isCorrect;
     char letter;
     do
@@ -34,13 +43,12 @@ do
     } while (!isCorrect);
     pendu.TestChar(letter);
     hasWin = pendu.TestWin();
-    Console.WriteLine("hasWin : " + hasWin + " | " + pendu.NbEssais);
 } while (!hasWin && pendu.NbEssais > 0);
 
 if (hasWin)
 {
-    Console.WriteLine("Gagné");
+    WriteInColor("Gagné", ConsoleColor.Green);
 } else
 {
-    Console.WriteLine("Perdu");
+    WriteInColor("Perdu", ConsoleColor.Red);
 }
