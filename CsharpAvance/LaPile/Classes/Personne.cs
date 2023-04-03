d3517d518ad721cea1eca1fcbc62ac69ff6b3452
Maxime Maillot <maillot.maxime62@gmail.com>
2023-04-03 11:50:44 +0200

    add pile

---
+++ CsharpAvance/LaPile/Classes/Personne.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaPile.Classes
+{
+    internal class Personne
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public Personne(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"Personne : {FirstName} {LastName} à {Age}";
+        }
+    }
+}
---
+++ CsharpAvance/LaPile/Classes/Pile.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaPile.Classes
+{
+    internal class Pile<T>
+    {
+        List<T> items = new List<T> ();
+
+        public void Push (T item)
+        {
+            Console.WriteLine($"Add : {item}");
+            items.Add(item);
+        }
+
+        public void Pop ()
+        {
+            Console.WriteLine($"Remove : {items[items.Count - 1]}");
+            items.RemoveAt(items.Count - 1);
+        }
+
+        public object Get (int index)
+        {
+            T item = items[index];
+            items.RemoveAt(index);
+            return item;
+        }
+
+        public override string ToString()
+        {
+            return $"Pile : {base.ToString()}";
+        }
+    }
+}
