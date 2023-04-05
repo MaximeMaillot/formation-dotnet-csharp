5ad10cd17c9e084ad80661761c4bf8b9e725d533
Maxime Maillot <maillot.maxime62@gmail.com>
2023-04-05 16:57:42 +0200

    test of struct

---
+++ CsharpAvance/Menu/Struct/Options.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Menu.Struct;
+public struct Options
+{
+    public Options(int[] different = null, int min = int.MinValue, int max = int.MaxValue)
+    {
+        Different = different;
+        Min = min;
+        Max = max;
+    }
+
+    public int[] Different { get; }
+    public int Min { get; }
+    public int Max { get; }
+
+    public bool DifferentIsNull()
+    {
+        return Different == null || Different.Length == 0;
+    }
+
+    public bool MinIsSet()
+    {
+        return Min != int.MinValue;
+    }
+
+    public bool MaxIsSet()
+    {
+        return Max != int.MaxValue;
+    }
+}
